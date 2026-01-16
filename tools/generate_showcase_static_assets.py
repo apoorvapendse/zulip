a0abef1cc21ec0a40d04aae497feb9e6f974490a
apoorvapendse <apoorvavpendse@gmail.com>
2026-01-16 22:39:11 +0530

    apoorva: build missing stuff for GH actions
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- tools/generate_showcase_static_assets.py
+++ tools/generate_showcase_static_assets.py
@@ -1,9 +1,23 @@
 import subprocess
 
 subprocess.run(
+    ["./tools/setup/emoji/build_emoji"],
+    check=True,
+)
+subprocess.run(
+    ["./tools/setup/build_pygments_data"],
+    check=True,
+)
+subprocess.run(
+    ["./tools/setup/build_timezone_values"],
+    check=True,
+)
+subprocess.run(["node", "tools/setup/build_supported_browser_regex.ts"])
+subprocess.run(
     ["./tools/webpack", "--config-name=frontend"],
     check=True,
 )
+
 
 # fake a favico icon
 subprocess.run(
